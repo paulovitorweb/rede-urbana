6ae0fbc39f8c28098d33990e9aa431d2416a8969
Paulo <paulogeo5@gmail.com>
2020-01-28 23:36:56 -0300

    Adiciona opção de usar centróides

--- scripts-pyqgis.py
+++ scripts-pyqgis.py
@@ -11,7 +11,7 @@ def add_qtde_lotes_field(layer, field_name="RU_Qtde"):
         layer.updateFields()
     return layer.fields().names().index(field_name)
 
-def run_qtde_lotes(metros):
+def run_qtde_lotes(metros, use_centroid=False):
     """ Calcula a quantidade de lotes que estão a um raio de <metros> metros 
     de cada lote, armazena num campo da tabela de atributos e retorna uma lista """
 
@@ -23,15 +23,26 @@ def run_qtde_lotes(metros):
     value = []
     layer.startEditing()
     numero = len(list(layer.getSelectedFeatures()))
-    print("Calculando para {} lotes...".format(numero))
-    for f in layer.getSelectedFeatures():
-        a = 0
-        for ff in layer.getSelectedFeatures():
-            # Se não for o mesmo lote e a geometria intersectar, incrementa
-            if f.id() != ff.id() and f.geometry().buffer(metros, 10).intersects(ff.geometry()):
-                a = a + 1
-        value.append([f.id(), a])
-        layer_provider.changeAttributeValues({f.id():{index: a}})
+    print("Calculando para {} lotes{}..."
+        .format(numero, " utilizando centróides" if use_centroid else " utilizando polígonos"))
+    if use_centroid:
+        for f in layer.getSelectedFeatures():
+            a = 0
+            for ff in layer.getSelectedFeatures():
+                # Se não for o mesmo lote e a geometria intersectar, incrementa
+                if f.id() != ff.id() and f.geometry().centroid().buffer(metros, 10).intersects(ff.geometry()):
+                    a = a + 1
+            value.append([f.id(), a])
+            layer_provider.changeAttributeValues({f.id():{index: a}})
+    else:
+        for f in layer.getSelectedFeatures():
+            a = 0
+            for ff in layer.getSelectedFeatures():
+                # Se não for o mesmo lote e a geometria intersectar, incrementa
+                if f.id() != ff.id() and f.geometry().buffer(metros, 10).intersects(ff.geometry()):
+                    a = a + 1
+            value.append([f.id(), a])
+            layer_provider.changeAttributeValues({f.id():{index: a}})
     layer.commitChanges()
     elapsed = timer() - inicio
     print("Cálculo efetuado com sucesso para {} lotes em {} segundos.".format(numero, round(elapsed, 4)))

4cc89e7c2406db893fe0714e514da8e64351551a
Paulo <paulogeo5@gmail.com>
2020-01-28 22:39:57 -0300

    Corrige função de adicionar field

--- scripts-pyqgis.py
+++ scripts-pyqgis.py
@@ -1,21 +1,21 @@
-def add_qtde_lotes_field(layer):
-    field_name = "RU_Qtde"
-    fields = layer.fields().names()
-    if field_name not in fields:
+def add_qtde_lotes_field(layer, field_name="RU_Qtde"):
+    """ Adiciona campo para armazenar a informação, caso não exista, 
+    e retorna o índice """
+    if field_name not in layer.fields().names():
         from PyQt5.QtCore import QVariant
         layer_provider = layer.dataProvider()
         layer_provider.addAttributes([QgsField(field_name, QVariant.Int)])
         layer.updateFields()
-    return fields.index(field_name)
+    return layer.fields().names().index(field_name)
 
 def run_qtde_lotes(metros):
-    """ Calcula a quantidade de lotes que estão a 
-    um raio de <metros> metros de cada lote """
-    
+    """ Calcula a quantidade de lotes que estão a um raio de <metros> metros 
+    de cada lote, armazena num campo da tabela de atributos e retorna uma lista """
+
+    field_name = "{}m".format(str(metros))
     layer = qgis.utils.iface.activeLayer()
-    index = add_qtde_lotes_field(layer)
+    index = add_qtde_lotes_field(layer, field_name)
     layer_provider = layer.dataProvider()
-    # features = layer.getSelectedFeatures()
     value = []
     layer.startEditing()
     for f in layer.getSelectedFeatures():
